e84e3a137472f92818c497b9dd04e1ed25a26900
TheComputerCat <juan.sanchez@sonatype.com>
2024-12-09 18:40:35 -0500

    File for project added

---
+++ Proyecto/pre-proyecto.jl
@@ -0,0 +1,13 @@
+### A Pluto.jl notebook ###
+# v0.20.3
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 60412e5c-b686-11ef-156c-b1e1858bb3d1
+md"""
+# Pre-proyecto
+"""
+
+# ╔═╡ Cell order:
+# ╠═60412e5c-b686-11ef-156c-b1e1858bb3d1
